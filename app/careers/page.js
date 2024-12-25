66418a9849416de4d009f582ab310bdbdc43101c
Abdul Haseeb Hamid <111380595+haseebx16@users.noreply.github.com>
2024-12-25 19:51:30 +0500

    Create page.js

---
+++ app/careers/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../Components/Sections/Nav'
+import Footer from '../Components/Sections/Footer'
+
+const page = () => {
+  return (
+    <div>
+        <Nav/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default page
