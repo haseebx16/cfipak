a1ded9ffe956b8294075c35ed83b68d0b73fca1f
Abdul Haseeb Hamid <111380595+haseebx16@users.noreply.github.com>
2024-12-25 19:47:55 +0500

    Update ContactForm.js

--- app/Components/Sections/ContactForm.js
+++ app/Components/Sections/ContactForm.js
@@ -54,7 +54,7 @@ const ContactForm = () => {
         {/* Right Column */}
         <div style={{ flex: "1 1 45%", minWidth: "300px" }}>
           <textarea
-            placeholder="Your Message.."
+            placeholder="Your Message."
             style={{
               width: "100%",
               height: "120px",
